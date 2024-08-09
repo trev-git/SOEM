9c39dc860e4c527f6a9b8b20286c48f5c74f80ae
trev-git <htaedshararam@gmail.com>
2024-08-09 15:15:38 +0300

    improve the sdo wrapper functions

--- test/linux/simple_test/sdo.h
+++ test/linux/simple_test/sdo.h
@@ -4,11 +4,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-uint8_t sdo_read8(uint16_t index, uint8_t subindex);
-uint16_t sdo_read16(uint16_t index, uint8_t subindex);
-uint32_t sdo_read32(uint16_t index, uint8_t subindex);
-bool sdo_write8(uint16_t index, uint8_t subindex, uint8_t val);
-bool sdo_write16(uint16_t index, uint8_t subindex, uint16_t val);
-bool sdo_write32(uint16_t index, uint8_t subindex, uint32_t val);
+uint8_t sdo_read8(int slave, uint16_t index, uint8_t subindex);
+uint16_t sdo_read16(int slave, uint16_t index, uint8_t subindex);
+uint32_t sdo_read32(int slave, uint16_t index, uint8_t subindex);
+bool sdo_write8(int slave, uint16_t index, uint8_t subindex, uint8_t val);
+bool sdo_write16(int slave, uint16_t index, uint8_t subindex, uint16_t val);
+bool sdo_write32(int slave, uint16_t index, uint8_t subindex, uint32_t val);
 
 #endif // SDO_H
